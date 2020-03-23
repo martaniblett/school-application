f16a0168d2952bf8239b19446a51669a5047d05d
martaniblett <martaniblett@gmail.com>
2020-03-23 22:01:42 +0000

    CRUD working

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 
 from application import app, db
 from application.forms import ClassForm, ClassUpdateForm, StaffForm, StaffUpdateForm
@@ -27,32 +27,34 @@ def staff_register():
     return render_template('staff.html', title='Staff Register', form=form)
 
 
-@app.route('/staff/update/<last_name>', methods=['GET', 'POST'])
-def staff_update(last_name):
+@app.route('/staff/update/<int:staff_id>', methods=['GET', 'POST'])
+def staff_update(staff_id):
     form = StaffUpdateForm() 
-    update_staff=Staff.query.filter_by(last_name=last_name).first()
+    update_staff=Staff.query.filter_by(id=staff_id).first()
     if form.validate_on_submit():
-         staffData = Staff(        
-            first_name = form.first_name.data,
-            last_name = form.last_name.data,
-            email = form.email.data,
-            DBS_status = form.DBS_status.data
-            )
+                
+        update_staff.first_name = form.first_name.data,
+        update_staff.last_name = form.last_name.data,
+        update_staff.email = form.email.data,
+        update_staff.DBS_status = form.DBS_status.data
+        
 
-         db.session.add(staffData)
-         db.session.commit()
-   
-    return redirect(url_for('home'))
-    
-    
-                 
+        db.session.commit()
+        return redirect(url_for('home'))
+    elif request.method == 'GET':
+        form.first_name.data = update_staff.first_name
+        form.last_name.data = update_staff.last_name
+        form.email.data = update_staff.email
+        form.DBS_status.data = update_staff.DBS_status
+
+                     
     return render_template('staff.html', title='Staff Register', form=form)
 
         
-@app.route('/staff/delete/<last_name>', methods=['GET', 'POST'])
-def staff_delete(last_name):
+@app.route('/staff/delete/<int:staff_id>', methods=['GET', 'POST'])
+def staff_delete(staff_id):
 
-    staff_delete=Staff.query.filter_by(last_name=last_name).first()
+    staff_delete=Staff.query.filter_by(id=staff_id).first()
     
 
     db.session.delete(staff_delete)
